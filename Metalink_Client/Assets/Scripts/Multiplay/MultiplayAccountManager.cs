069d4e52e71d4418414bc315909eb68de33e1f9d
MolecularPaper <jat05026@gmail.com>
2022-09-14 14:04:42 +0900

    Update

--- Metalink_Client/Assets/Scripts/Multiplay/MultiplayAccountManager.cs
+++ Metalink_Client/Assets/Scripts/Multiplay/MultiplayAccountManager.cs
@@ -20,10 +20,18 @@ namespace Multiplay
             }
         }
 
+        private string _playFabPlayerIdCache;
+
         public void SignUp(string emailAdress, string password, string username, UnityAction<bool, string> callback = null)
         {
             var request = new RegisterPlayFabUserRequest { Email = emailAdress, Password = password, Username = username };
-            PlayFabClientAPI.RegisterPlayFabUser(request, val => OnSingUpSucces(val, callback), val => OnSingUpFailuer(val, callback));
+            PlayFabClientAPI.RegisterPlayFabUser(request, val => OnSingUpSucces(val, callback), val => OnPlayFabError(val, callback));
+        }
+
+        public void LogIn(string emailAdress, string password, UnityAction<bool, string> callback = null)
+        {
+            var request = new LoginWithEmailAddressRequest { Email = emailAdress, Password = password };
+            PlayFabClientAPI.LoginWithEmailAddress(request, val => OnLogInSucces(val, callback), val => OnPlayFabError(val, callback));
         }
 
         public void OnSingUpSucces(RegisterPlayFabUserResult registerPlayFabUserResult, UnityAction<bool, string> successCallback)
@@ -32,28 +40,39 @@ namespace Multiplay
             Debug.Log("회원가입 성공");
         }
 
-        public void OnSingUpFailuer(PlayFabError playFabError, UnityAction<bool, string> failedCallback)
-        {
-            failedCallback?.Invoke(false, "SingUp Failed, " + playFabError.ErrorMessage);
-            Debug.Log("회원가입 실패, " + playFabError.ErrorMessage);
-        }
-
-        public void LogIn(string emailAdress, string password, UnityAction<bool, string> callback = null)
-        {
-            var request = new LoginWithEmailAddressRequest { Email = emailAdress, Password = password };
-            PlayFabClientAPI.LoginWithEmailAddress(request, val => OnLogInSucces(val, callback), val => OnLogInFailuer(val, callback));
-        }
-
         public void OnLogInSucces(LoginResult loginResult, UnityAction<bool, string> successCallback)
         {
             successCallback?.Invoke(true, "LogIn Succes");
+            RequestPhotonToken(loginResult);
             Debug.Log("로그인 성공");
+
         }
 
-        public void OnLogInFailuer(PlayFabError playFabError, UnityAction<bool, string> failedCallback)
+        public void OnPlayFabError(PlayFabError playFabError, UnityAction<bool, string> failedCallback = null)
         {
-            failedCallback?.Invoke(false, "LogIn Failed, " );
-            Debug.Log("로그인 실패, " + playFabError.ErrorMessage);
+            failedCallback?.Invoke(false, playFabError.ErrorMessage);
+            Debug.Log(playFabError.ErrorMessage);
+        }
+
+        private void RequestPhotonToken(LoginResult obj)
+        {
+            Debug.Log("PlayFab authenticated. Requesting photon token...");
+            _playFabPlayerIdCache = obj.PlayFabId;
+
+            PlayFabClientAPI.GetPhotonAuthenticationToken(new GetPhotonAuthenticationTokenRequest() {
+                PhotonApplicationId = PhotonNetwork.PhotonServerSettings.AppID
+            }, AuthenticateWithPhoton, val => OnPlayFabError(val));
+        }
+
+        private void AuthenticateWithPhoton(GetPhotonAuthenticationTokenResult obj)
+        {
+            Debug.Log("Photon token acquired: " + obj.PhotonCustomAuthenticationToken + "  Authentication complete.");
+
+            var customAuth = new AuthenticationValues { AuthType = CustomAuthenticationType.Custom };
+            customAuth.AddAuthParameter("username", _playFabPlayerIdCache);
+            customAuth.AddAuthParameter("token", obj.PhotonCustomAuthenticationToken);
+
+            PhotonNetwork.AuthValues = customAuth;
         }
     }
 }
